e290627cc367019c75615c3307b5ca2f0a1ea8ba
Mahesh Sabnis <sabnis_m@hotmail.com>
2021-10-09 17:36:12 +0530

    Add project files.

---
+++ COnsoleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ClientNS; 
+namespace COnsoleClient
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			HttpClient client = new HttpClient();
+			RequestClient apiClient = new RequestClient("https://localhost:44304/", client);
+			var res = await apiClient.GetallAllAsync();
+			Console.WriteLine($"Result = {JsonSerializer.Serialize(res)}");
+			Console.ReadLine();
+		}
+	}
+}
